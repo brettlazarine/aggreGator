273b8d39ff79a39bf744aa49afdfe06bf16f33ad
brettlazarine <jblazarine@gmail.com>
2025-01-04 21:55:14 -0600

    fetch xml with agg, parse and print

---
+++ handler_agg.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func handlerAgg(s *state, cmd command) error {
+	url := "https://www.wagslane.dev/index.xml"
+	feed, err := fetchFeed(context.Background(), url)
+	if err != nil {
+		return fmt.Errorf("error fetching feed: %v", err)
+	}
+
+	fmt.Printf("Feed: %s\n", feed)
+	return nil
+}
