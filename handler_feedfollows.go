4f74c1b6c72ef1ec6856228a0c27db5a05d8488e
brettlazarine <jblazarine@gmail.com>
2025-01-06 12:03:55 -0600

    unfollow feed

--- handler_feedfollows.go
+++ handler_feedfollows.go
@@ -56,3 +56,26 @@ func handlerFollowing(s *state, cmd command, user database.User) error {
 
 	return nil
 }
+
+func handlerUnfollow(s *state, cmd command, user database.User) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("unfollow requires a url -> %v <url>", cmd.Name)
+	}
+	url := cmd.Args[0]
+
+	feed, err := s.db.GetFeedByUrl(context.Background(), url)
+	if err != nil {
+		return fmt.Errorf("error getting feed: %v", err)
+	}
+
+	err = s.db.DeleteFeedFollow(context.Background(), database.DeleteFeedFollowParams{
+		UserID: user.ID,
+		FeedID: feed.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("error deleting feed follow: %v", err)
+	}
+
+	fmt.Printf("*** Feed %v unfollowed for user %v\n", feed.Name, user.Name)
+	return nil
+}
